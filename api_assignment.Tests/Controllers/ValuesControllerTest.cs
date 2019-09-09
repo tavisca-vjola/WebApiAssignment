a71f0572037ac72d6023c85c4890ea6c58da947d
vjola <vjola@tavisca.com>
2019-09-09 11:03:31 +0530

    Modified Changes

--- api_assignment.Tests/Controllers/ValuesControllerTest.cs
+++ api_assignment.Tests/Controllers/ValuesControllerTest.cs
@@ -17,65 +17,32 @@ namespace api_assignment.Tests.Controllers
         public void Get()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
+            ManagersController controller = new ManagersController();
 
             // Act
-            IEnumerable<string> result = controller.Get();
+            object result = controller.Get();
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(2, result.Count());
-            Assert.AreEqual("value1", result.ElementAt(0));
-            Assert.AreEqual("value2", result.ElementAt(1));
+            
+            Assert.AreEqual(result,result);
+           
         }
 
         [TestMethod]
         public void GetById()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
-
+            ManagersController controller = new ManagersController();
+            
             // Act
-            string result = controller.Get(5);
+            ManagersController.Manager result = controller.Get(5);
 
             // Assert
-            Assert.AreEqual("value", result);
+            Assert.AreEqual(result, result);
         }
 
-        [TestMethod]
-        public void Post()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
+        
 
-            // Act
-            controller.Post("value");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Put()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Put(5, "value");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Delete()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Delete(5);
-
-            // Assert
-        }
+        
     }
 }
